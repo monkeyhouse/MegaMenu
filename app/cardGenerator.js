a7f307eab231ac1cd4e9469adba460b84ad0a420
Sam Prager <sam.prager@gmail.com>
2015-01-31 20:44:31 -0600

    Initial Commit
    Add all files

---
+++ app/cardGenerator.js
@@ -0,0 +1,48 @@
+﻿//dependency on lorem impsum generator & zencoding
+
+
+var cardGenerator = (function () {
+
+    var api = {};
+
+    api.createCard = createSimpleCard;
+    api.createCards = createNCards;
+
+    function createSimpleCard() {
+
+        var minItems = 4;
+        var maxItems = 15;
+        if (Math.random() < .4) {
+            maxItems = 120;
+        }
+        var numItems = minItems + Math.floor(Math.random() * (maxItems - minItems)) + 1;
+
+        var title = lipsum.generateWords(3);
+        var words = lipsum.generateWordsArray(5, numItems).map(function (w) { return { 'word': w }; });
+        var data = {
+            title: title,
+            words: words
+        };
+        var template = '' +
+            //'div.ui.vertical.menu>' +
+            'div.item>((b>{!title!})' + // !words.length!
+            '+(div.menu>!for:words!' +
+            'a.item{!word!}))';
+
+        var myOutput = $.zc(template, data);
+
+        return myOutput.get(0);
+    }
+
+    function createNCards(n) {
+        n = n || 7;
+        var c = document.createDocumentFragment();
+        for (var i = 0; i < n; i++) {
+            var card = createSimpleCard();
+            c.appendChild(card);
+        }
+        return c;
+    }
+
+    return api;
+})();
---
+++ app/ddnLayout/layout.js
@@ -0,0 +1,33 @@
+﻿$(function() {
+
+    $('.item').click(function() {
+        var items = $(this).parent('.menu').find('.item');
+        items.removeClass('active');
+        $(this).addClass('active');
+    });
+
+
+    $(function() {
+        // in app, not all menu options are on top, also, they actually have unique urls
+        var tOpts = document.createDocumentFragment();
+        $('#topnav a').each(function(ix, el) {
+            tOpts.appendChild(new Option(el.innerText, el.getAttribute('href') + ix));
+        });
+        $('#mtopnav').append(tOpts);
+
+        //
+        var sOpts = document.createDocumentFragment();
+        $('#sidenav a').each(function(ix, el) {
+            sOpts.appendChild(new Option(el.innerText, el.getAttribute('href') + ix));
+        });
+        $('#msidenav').append(sOpts);
+
+        $('.ui.dropdown')
+            .dropdown({
+                // you can use any ui transition
+                transition: 'drop'
+            });
+
+    });
+
+});
